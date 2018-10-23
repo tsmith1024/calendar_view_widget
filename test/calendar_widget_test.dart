360e90ada0cf8485862c92c338eea3f36f3a1e5d
Taylor Smith <taylorsmith@Taylors-MacBook-Pro.local>
2018-10-23 10:44:07 -0500

    Comment out unused tests

--- test/calendar_widget_test.dart
+++ test/calendar_widget_test.dart
@@ -1,7 +1,4 @@
-import 'package:test/test.dart';
-
-import 'package:calendar_view_widget/calendar_view_widget.dart';
-
-void main() {
-
-}
+// import 'package:test/test.dart';
+// import 'package:calendar_view_widget/calendar_view_widget.dart';
+// void main() {
+// }
